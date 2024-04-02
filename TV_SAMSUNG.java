104883ed4265dc0126a42c9b684931cc789da52c
Trọng Vỹ <hotrongvi1101@gmail.com>
2024-04-02 21:59:35 +0700

    Alo

--- TV_SAMSUNG.java
+++ TV_SAMSUNG.java
@@ -1,5 +1,55 @@
 package project;
 
-public class TV_SAMSUNG {
+public class TV_SAMSUNG extends TV{
+    private String state;
 
+    public TV_SAMSUNG() {
+    }
+
+    public TV_SAMSUNG(String state, String tvID, String manifacturer, Date entryDate, double price, double num) {
+        super(tvID, manifacturer, entryDate, price, num);
+        this.state = state;
+    }
+
+    @Override
+    public void Input(){
+        super.Input();
+        System.out.print("Moi nhap State (new or old): ");
+        sc.nextLine();
+        state = sc.nextLine();
+    }
+    
+    @Override
+    public void Output(){
+        super.Output();
+        System.out.println("State: "+state);
+        System.out.println("Total: "+sum_Total());
+        System.out.println("Discount: "+discount());
+    }
+    
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+    
+    @Override
+    public double sum_Total(){
+        return getNum()*getPrice()-discount();
+    }
+    
+    @Override
+    public double discount(){
+        if("new".equals(state)){
+            return getNum()*getPrice()*10/100;
+        }
+        else if("old".equals(state)){
+            return getNum()*getPrice()*60/100;
+        }
+        else return 0;
+    }
 }
+
+
